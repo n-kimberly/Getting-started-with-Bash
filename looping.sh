e92f24cf07e9b0e98010e69099119f8fc2d52978
nykimberly <kimberlyvnguyen@gmail.com>
2018-08-11 22:05:13 -0700

    simple input, ifs, loops, operations

---
+++ looping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Your task is to use for loops to display only odd natural numbers from 1 to 99.
+for value in {1..100..2}
+do
+    echo $value
+done
+
+#Use for loops to display the natural numbers from 1 to 50
+for value in {1..50}
+do
+	echo $value
+done
